2d4b79bad4c8ccb92cc8b9811df0bf875ebc4ea2
AkimotoAkari <mahoo2d@yahoo.co.jp>
2016-12-22 22:02:35 +0800

    i18n.js修改
    昨晚饭堂抢汤圆的人从头排到尾，就为了8个汤圆

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,8 +1,14 @@
 /* events: i18nready, loaderror */
+/* MyDiary/i18n.js
+ * Version 0.0.2.1218
+ */
 
 window.i18n = {};
 
 i18n.data = {};
+
+i18n.default_language = "ja-jp",
+i18n.user_language = navigator.language.toLowerCase();
 
 i18n.load = function( data_url ) {
 	var xhr = new XMLHttpRequest;
@@ -11,6 +17,12 @@ i18n.load = function( data_url ) {
 	xhr.onreadystatechange = function( ) {
 		if( xhr.readyState === 4 && xhr.status === 200 ){
 			i18n.data = JSON.parse(xhr.responseText);
+			_fire("i18nready");
 		}
 	}
-}
+};
+i18n.get = function( name, lang ) {
+	if( !lang ) lang = i18n.user_language;
+	return ( ( i18n.data[ lang ] ? ( i18n.data[ lang ][name] || void 0 ) : void 0 )  || i18n.data[ i18n.default_language ][name] )
+	|| "[i18n.get] Error: " + name + ", in " + lang + ".";
+};
