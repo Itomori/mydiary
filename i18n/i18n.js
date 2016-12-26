69628231ede451e9cbea2bd619d24b70b8b53df4
AkimotoAkari <mahoo2d@yahoo.co.jp>
2016-12-26 12:34:47 +0800

    i18n.js修改
    把一台电脑刷了ubuntu当服务器，以后在学校内网搞一个网站

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -12,10 +12,11 @@ i18n.user_language = navigator.language.toLowerCase();
 
 i18n.load = function( data_url ) {
 	var xhr = new XMLHttpRequest;
-	xhr.open( "GET", "i18n/i18n.txt", true );
+	xhr.open( "GET", data_url || "i18n/i18ntest.txt", true );
 	xhr.send();
 	xhr.onreadystatechange = function( ) {
 		if( xhr.readyState === 4 && xhr.status === 200 ){
+			console.log(xhr);
 			i18n.data = JSON.parse(xhr.responseText);
 			_fire("i18nready");
 		}
@@ -23,6 +24,8 @@ i18n.load = function( data_url ) {
 };
 i18n.get = function( name, lang ) {
 	if( !lang ) lang = i18n.user_language;
-	return ( ( i18n.data[ lang ] ? ( i18n.data[ lang ][name] || void 0 ) : void 0 )  || i18n.data[ i18n.default_language ][name] )
-	|| "[i18n.get] Error: " + name + ", in " + lang + ".";
+	var ret = "[Error:i18n.get()] Trying to get '" + name + "'; but nothing.";
+	if ( i18n.data[lang] ) 
+		ret = i18n.data[lang][name] || i18n.data[i18n.default_language][name];
+	return ret;
 };
